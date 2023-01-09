aa72e9e6dde544f9ecc1df4b599c4a39ac6e4536
uday bhardwaj <103742193+Udaybhardwaj179@users.noreply.github.com>
2023-01-09 21:03:24 +0530

    Create Binary Tree Postorder Traversal.cpp

---
+++ Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+vector<int> v;
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        if(root){
+            v.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+        return v;
+
+    }
+};
